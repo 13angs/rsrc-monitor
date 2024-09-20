16ead3df052bf687ac137a4c76f744cb3768ac1d
13angs <13angz.um@gmail.com>
2024-09-20 01:16:16 +0000

    Move the db context into repo

--- src/alerting.py
+++ src/alerting.py
@@ -1,48 +1,28 @@
 from collections import defaultdict
-import psycopg2
-from datetime import datetime
 from telegram import Bot
 import asyncio
 from my_env import db_params, telegram_bot_config
+from db.fuel_repo import FuelRepsitory
+from db.db_context import DatabaseContext
 
 # Telegram Bot Token and Chat ID (replace these with your own)
 TELEGRAM_BOT_TOKEN = telegram_bot_config['token']
 TELEGRAM_CHAT_ID = telegram_bot_config['chat_id']
 
-# Connect to PostgreSQL database
-# Connect to PostgreSQL database
-def connect_db():
-    try:
-        conn = psycopg2.connect(**db_params)
-        return conn
-    except Exception as e:
-        print(f"Error connecting to the database: {e}")
-        return None
-
 # Query fuel prices for specific types and today's date
 def get_fuel_prices():
-    conn = connect_db()
-    if not conn:
-        return None
-
-    today_date = datetime.now().strftime('%Y-%m-%d')
-    query = """
-    SELECT provider, type, price
-    FROM fuel_prices
-    WHERE type IN ('แก๊สโซฮอล์ 95', 'แก๊สโซฮอล์ E20', 'ดีเซล B7')
-    AND date = %s
-    ORDER BY provider, type;
-    """
-    
+    db_context = DatabaseContext(db_params)
+    fuel_repo = FuelRepsitory(db_context)
+    db_context.connect()
     try:
-        cur = conn.cursor()
-        cur.execute(query, (today_date,))
-        rows = cur.fetchall()
-        conn.close()
-        return rows
+
+        return fuel_repo.get_fuel_prices()
     except Exception as e:
         print(f"Error querying the database: {e}")
         return None
+    finally:
+        db_context.close()
+
 
 # Format the fuel prices by grouping them by type
 def format_fuel_prices(prices):
@@ -56,13 +36,13 @@ def format_fuel_prices(prices):
 
     # Create a formatted message
     message = "🚗 Fuel Prices for Today:\n\n"
-    
+
     for fuel_type, entries in grouped_data.items():
         message += f"🔹 {fuel_type}:\n"
         for provider, price in entries:
             message += f"  - {provider}: {price:.2f} THB\n"
         message += "\n"
-    
+
     return message
 
 # Send the message to Telegram (async function)
--- src/db/fuel_repo.py
+++ src/db/fuel_repo.py
@@ -1,6 +1,7 @@
 from db.db_context import DatabaseContext
 from datetime import datetime
 
+
 class FuelRepsitory:
     def __init__(self, db_manager: DatabaseContext):
         """Initialize with a reference to the generic Repsitory"""
@@ -22,12 +23,28 @@ class FuelRepsitory:
     def insert_fuel_data(self, fuel_data: list):
         """Insert fuel prices into the database"""
         current_date = datetime.now().strftime('%Y-%m-%d')
-        
+
         insert_query = '''
         INSERT INTO fuel_prices (date, provider, type, price)
         VALUES (%s, %s, %s, %s)
         '''
-        
+
         for entry in fuel_data:
-            self.db_manager.execute(insert_query, (current_date, entry['provider'], entry['type'], entry['price']))
+            self.db_manager.execute(
+                insert_query, (current_date, entry['provider'], entry['type'], entry['price']))
         print(f"Fuel data inserted successfully.")
+
+    def get_fuel_prices(self, fuel_type=('แก๊สโซฮอล์ 95', 'แก๊สโซฮอล์ E20', 'ดีเซล B7')):
+        today_date = datetime.now().strftime('%Y-%m-%d')
+
+        query = """
+        SELECT provider, type, price
+        FROM fuel_prices
+        WHERE type IN %s
+        AND date = %s
+        ORDER BY provider, type;
+        """
+
+        self.db_manager.execute(query, (fuel_type, today_date,))
+        rows = self.db_manager.cursor.fetchall()
+        return rows
