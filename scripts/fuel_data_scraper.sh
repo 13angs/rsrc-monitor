e10dbfbdb1882dfc79de44ff802b61274ff52f15
13angs <13angz.um@gmail.com>
2024-09-25 23:47:37 +0000

    Run the app using docker-compose

--- scripts/fuel_data_scraper.sh
+++ scripts/fuel_data_scraper.sh
@@ -1,3 +1,15 @@
 #!/bin/bash
 
-docker-compose -f /home/bangz/apps/rsrc-monitor/config/docker-compose.prod.yaml up fuel
+# docker-compose -f /home/bangz/apps/rsrc-monitor/config/docker-compose.prod.yaml up fuel
+
+# Get the container ip
+export CONTAINER_IP=$(docker inspect -f '{{range .NetworkSettings.Networks}}{{.IPAddress}}{{end}}' config-rsrc-monitor-1)
+echo "Container IP is $CONTAINER_IP."
+echo
+
+echo "Scraping the fuel data..."
+response=$(curl http://$CONTAINER_IP:8000/api/scrape/fuel)
+echo
+
+status=$(echo "$response" | jq '.status')
+echo "Status is $status"
